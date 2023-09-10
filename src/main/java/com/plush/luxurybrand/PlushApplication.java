a947dee9fe0faebeb1932509885dba390b33cf2c
Geniece Tims <geniece.c.tims@gmail.com>
2023-09-09 23:52:38 -0500

    first commit

---
+++ src/main/java/com/plush/luxurybrand/PlushApplication.java
@@ -0,0 +1,13 @@
+package com.plush.luxurybrand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlushApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlushApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/plush/luxurybrand/PlushApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plush.luxurybrand;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlushApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
